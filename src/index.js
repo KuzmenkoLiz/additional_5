638978b6ee9be81eca1f49d7c0ff8a6ad74ac9ef
KuzmenkoLiz <lizaveta330@gmail.com>
2018-02-25 00:43:30 +0300

    sixth task: right version

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // my solution
+
   var mass = [];
 
-  for (var i = 0; i < str.lenght; i++) {
-    for (var j = 0; j < bracketsConfig.lenght; j++) {
-      if (str.charAt(i) == bracketsConfig[j][0] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
-        if (str.charAt(i) == mass[mass.lenght-1]) {
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][1] == bracketsConfig[j][0]) {
+        if (str[i] == mass[mass.length-1]) {
           mass.pop();
         } else {
-          mass.push(str.charAt(i));
-        }
-      } /**/
-      if (str.charAt(i) == bracketsConfig[j][0]) {
-        mass.push(str.charAt(i));
-      } 
-      if (str.charAt(i) == bracketsConfig[j][1]) {
-        if (mass.pop() != bracketsConfig[j][0]) {
-         return false;
+          mass.push(str[i]);
         }
       }
-    } 
+      if (str[i] == bracketsConfig[j][0]) {
+        mass.push(str[i]);
+      }
+      if (str[i] == bracketsConfig[j][1]) {
+        if (mass.pop() != bracketsConfig[j][0]) {
+          return false;
+         }
+      }
+    }
   }
   return mass.length == 0 ? true : false;
 }
